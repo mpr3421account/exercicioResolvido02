791e419967a5d16adb8db2d33129d896126717b2
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2022-12-27 08:52:17 -0300

    StringBuilder

--- Entities/Post.cs
+++ Entities/Post.cs
@@ -30,5 +30,21 @@ namespace exercicioResolvido02.Entities
         {
             Comments.Remove(comment);
         }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments:");
+            foreach(Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,6 +8,16 @@ Post p1 = new Post(
     "Traveling to New Zealand",
     "I'm going to visit the wonderful country!",
     12);
-
 p1.AddComment(c1);
 p1.AddComment(c2);
+
+Comment c3 = new Comment("Good night");
+Comment c4 = new Comment("May the Force be with you");
+
+Post p2 = new Post(
+    DateTime.Parse("28/07/2018 23:14:19"),
+    "Good night guys",
+    "See you tomorrow",
+    5);
+p2.AddComment(c3);
+p2.AddComment(c4);
