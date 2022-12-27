2579919d9c3a0696602ea21f107bc14fbc21ca9f
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2022-12-27 08:34:56 -0300

    Inserido Classe Post

---
+++ Entities/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioResolvido02.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+    }
+}
